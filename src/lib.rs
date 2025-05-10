1cc50333736766406e268cdd3b3bbf703e9782cf
Ronie Martinez <2573537+roniemartinez@users.noreply.github.com>
2025-05-10 19:12:07 +0100

    ✨ MVP (#1)
    * ✨ MVP
    
    * Downgrade freedesktop-desktop-entry

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,67 @@
+use pyo3::Bound;
 use pyo3::prelude::*;
+use pyo3::types::PyDict;
+use rsbrowsers::{Browser, BrowserFinder};
+use std::convert::Infallible;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+struct PyBrowser(Browser);
+
+impl<'py> IntoPyObject<'py> for PyBrowser {
+    type Target = PyDict;
+    type Output = Bound<'py, PyDict>;
+    type Error = Infallible;
+
+    fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
+        let dict = PyDict::new(py);
+
+        dict.set_item("display_name", self.0.display_name)
+            .expect("Cannot set display_name.");
+        dict.set_item("path", self.0.path).expect("Cannot set path.");
+        dict.set_item("browser_type", self.0.browser_type)
+            .expect("Cannot set browser_type.");
+        dict.set_item("version", self.0.version).expect("Cannot set version.");
+
+        Ok(dict)
+    }
+}
+
+/// Iterates over installed browsers.
+#[pyfunction(name = "browsers")]
+fn all<'py>(py: Python<'py>) -> PyResult<Vec<PyBrowser>> {
+    let browsers = BrowserFinder::new().all().map(|browser| PyBrowser(browser)).collect();
+    Ok(browsers)
+}
+
+/// Returns the information for the provided browser key.
+#[pyfunction(signature = (browser, version="*"))]
+fn get<'py>(py: Python<'py>, browser: String, version: &str) -> PyResult<Option<PyBrowser>> {
+    match BrowserFinder::new()
+        .with_type(browser)
+        .with_version(version.to_string())
+        .all()
+        .next()
+    {
+        Some(browser) => Ok(Some(PyBrowser(browser))),
+        None => Ok(None),
+    }
+}
+
+/// Launches a web browser.
+#[pyfunction(signature = (browser, version=None, url=None, args=None))]
+fn launch(browser: String, version: Option<String>, url: Option<String>, args: Option<Vec<String>>) {
+    let mut finder = BrowserFinder::new().with_type(browser);
+    if let Some(v) = version {
+        finder = finder.with_version(v);
+    }
+    let args = args.unwrap_or_else(|| vec![]);
+    finder.launch(args.as_slice());
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn browsers(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(all, m)?)?;
+    m.add_function(wrap_pyfunction!(get, m)?)?;
+    m.add_function(wrap_pyfunction!(launch, m)?)?;
     Ok(())
 }
